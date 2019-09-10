da51af86db47972c4745e31996662e2da1a00b23
EvgenyBorisenko <borisenkoevgeny1991@gmail.com>
2019-09-10 15:38:09 +0300

    Updates

---
+++ precache-manifest.bbd8677fbc217ec347b3705066897a06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c40349eb9d93f0132ba797cb0026d1f",
+    "url": "/goit-react-hw-02-banking-components/index.html"
+  },
+  {
+    "revision": "67b4149e574f0a692a0e",
+    "url": "/goit-react-hw-02-banking-components/static/css/2.10583f31.chunk.css"
+  },
+  {
+    "revision": "8d527e04afd3180c5146",
+    "url": "/goit-react-hw-02-banking-components/static/css/main.cbc78744.chunk.css"
+  },
+  {
+    "revision": "67b4149e574f0a692a0e",
+    "url": "/goit-react-hw-02-banking-components/static/js/2.a33dccfc.chunk.js"
+  },
+  {
+    "revision": "8d527e04afd3180c5146",
+    "url": "/goit-react-hw-02-banking-components/static/js/main.13828224.chunk.js"
+  },
+  {
+    "revision": "a37689cb335e46cdab60",
+    "url": "/goit-react-hw-02-banking-components/static/js/runtime~main.4e4a5ca4.js"
+  }
+]);
